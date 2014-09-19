f325f54f28fd55a1efbaf31fadcb11a7fcbb1295
punksux <punksux2.0@gmail.com>
2014-09-19 14:59:28 -0600

    still cant figure out why the damn tab colors don't work

--- info.py
+++ info.py
@@ -16,17 +16,32 @@ icon = ""
 day = True
 sun_or_moon_icon = ''
 d_n_clouds = ''
-#rss_feed = 'http://www.kutv.com/news/features/top-stories/stories/rss.xml'
-rss_feed = 'http://feeds.abcnews.com/abcnews/topstories'
-feed = []
-feed_titles = []
-feed_summary = []
+rss_feed = 'http://www.kutv.com/news/features/top-stories/stories/rss.xml'
+#rss_feed = 'http://feeds.abcnews.com/abcnews/topstories'
+
 weather_website = ('http://api.wunderground.com/api/c5e9d80d2269cb64/conditions/astronomy/forecast10day/q/%s.json' %
                    location)
-forecastDay = []
-forecastCond = []
-forecastHigh = []
-forecastLow = []
+feed = []
+feed_titles = []
+feed_titles_old = []
+feed_summary = []
+feed_summary_old = []
+forecast_day = []
+forecast_day_old = []
+forecast_cond = []
+forecast_cond_old = []
+forecast_high = []
+forecast_high_old = []
+forecast_low = []
+forecast_low_old = []
+icon_old = []
+tom_temp = '0'
+tom_temp_old = '1'
+day_night_old = ''
+out_temp = '0'
+out_temp_old = '0'
+in_temp = '0'
+in_temp_old = '0'
 
 templateData = {
     'icon': ['', '', ''],
@@ -50,17 +65,16 @@ else:
 
 app = Flask(__name__)
 
-logging.basicConfig(filename='errors.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p')
+#logging.basicConfig(filename='errors.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s',
+                    #datefmt='%m/%d/%Y %I:%M:%S %p')
 
 sched = Scheduler()
 sched.start()
 
 
+#    --==RSS Stuff==--
 def get_rss():
-    global feed
-    global feed_titles
-    global feed_summary
+    global feed, feed_titles, feed_summary, feed_titles_old, feed_summary_old
     feed = feedparser.parse(rss_feed)
     feed['items'] = feed['items'][:10]
     for i in feed['items']:
@@ -73,8 +87,8 @@ rss = sched.add_interval_job(get_rss, seconds=5*60)
 
 
 def check_weather():
-    global icon
-    global precip
+    global icon, forecast_day, forecast_cond, forecast_high, forecast_low, forecast_day_old, forecast_cond_old
+    global forecast_high_old, forecast_low_old, tom_temp
     if weather_test == 200:
         global something_wrong
         global f
@@ -110,17 +124,38 @@ def check_weather():
 
             templateData['sunset_hour'] = parsed_json['sun_phase']['sunset']['hour']
             templateData['sunset_minute'] = parsed_json['sun_phase']['sunset']['minute']
+            for i in range(0, 5):
+                forecast_cond.append(parsed_json['forecast']['simpleforecast']['forecastday'][i]['icon'])
+                forecast_day.append(parsed_json['forecast']['simpleforecast']['forecastday'][i]['date']['weekday'])
+                forecast_high.append(parsed_json['forecast']['simpleforecast']['forecastday'][i]['high']['fahrenheit'])
+                forecast_low.append(parsed_json['forecast']['simpleforecast']['forecastday'][i]['low']['fahrenheit'])
 
+            tom_temp = parsed_json['forecast']['simpleforecast']['forecastday'][i]['high']['fahrenheit']
             sun_or_moon()
             set_icon()
             f.close()
     else:
-        #randt = ['rain', 'clear', 'partlycloudy', 'mostlycloudy', 'flurries']
-        randt = ['rain', 'clear']
-        icon = randt[random.randrange(0, 2)]
+        def rand_weather():
+            randt = ['rain', 'clear', 'partlycloudy', 'mostlycloudy', 'flurries']
+            return randt[random.randrange(0, 5)]
+
+        def get_rand():
+            return random.randrange(60, 90)
+
+        icon = rand_weather()
         sun_or_moon()
+
+        tom_temp = get_rand()
+        forecast_day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        forecast_cond = [rand_weather(), rand_weather(), rand_weather(), rand_weather(), rand_weather()]
+        forecast_high = [get_rand(), get_rand(), get_rand(), get_rand(), get_rand()]
+        forecast_low = [get_rand(), get_rand(), get_rand(), get_rand(), get_rand()]
+
+        # if templateData['day_night'] == 'day':
+        #     templateData['day_night'] = 'night'
+        # else:
+        #     templateData['day_night'] = 'day'
         set_icon()
-        precip = True
 
 
 def sun_or_moon():
@@ -135,13 +170,13 @@ def sun_or_moon():
         sun_or_moon_icon = 'sun'
         d_n_clouds = 'day'
         templateData['background'] = '-moz-linear-gradient(top, #C9E3FB 0%, #529BE4 100%);'
-        templateData['day_night'] = 'day'
+        #templateData['day_night'] = 'day'
     else:
         day = False
         sun_or_moon_icon = 'moon'
         d_n_clouds = 'night'
         templateData['background'] = '-moz-linear-gradient(top, #1f1f3f 0%, #141f31 100%);'
-        templateData['day_night'] = 'night'
+        #templateData['day_night'] = 'night'
 
 
 def set_icon():
@@ -172,22 +207,62 @@ def set_icon():
 check_weather()
 weather = sched.add_interval_job(check_weather, seconds=60)
 
+rss_once = False
 
+
+def get_temps_from_probes():
+    global out_temp, in_temp
+    out_temp = str(random.randrange(-32, 104))
+    in_temp = str(random.randrange(32, 104))
+
+get_temps_from_probes()
+temps = sched.add_interval_job(get_temps_from_probes, seconds=10)
+
+#     --==Streaming Stuff==--
 def event_stream():
-        yield 'event: outTemp\n' + \
-              'data: ' + (str(random.randrange(-32, 104))) + '\n\n' + \
-              'event: inTemp\n' + \
-              'data: ' + str(random.randrange(32, 104)) + '\n\n' + \
-              'event: time\n' + \
-              'data: ' + str(datetime.now().time().strftime('%I:%M %p').lstrip("0")) + '\n\n' + \
-              'event: rssTitle\n' + \
-              'data: ' + json.dumps(feed_titles) + '\n\n' + \
-              'event: rssSum\n' + \
-              'data: ' + json.dumps(feed_summary) + '\n\n' + \
-              'event: icon\n' + \
-              'data: ' + json.dumps(templateData['icon']) + '\n\n'
+    global icon, forecast_day, forecast_cond, forecast_high, forecast_low, forecast_day_old, forecast_cond_old
+    global forecast_high_old, forecast_low_old, icon_old, rss_once, feed_titles_old, feed_summary_old, tom_temp
+    global tom_temp_old, day_night_old, out_temp_old, in_temp_old
+    yield_me = ''
+    if templateData['day_night'] != day_night_old:
+        print(templateData['day_night'])
+        day_night_old = templateData['day_night']
+        yield_me += 'event: dayNight\n' + 'data: ' + templateData['day_night'] + '\n\n'
+    if out_temp != out_temp_old:
+        out_temp_old = out_temp
+        yield_me += 'event: outTemp\n' + 'data: ' + str(out_temp) + '\n\n'
+    if in_temp != in_temp_old:
+        in_temp_old = in_temp
+        yield_me += 'event: inTemp\n' + 'data: ' + str(in_temp) + '\n\n'
+    if tom_temp != tom_temp_old:
+        tom_temp_old = tom_temp
+        yield_me += 'event: tomTemp\n' + 'data: ' + str(tom_temp) + '\n\n'
+    if feed_titles != feed_titles_old or rss_once is False:
+        feed_titles_old = feed_titles
+        yield_me += 'event: rssTitle\n' + 'data: ' + json.dumps(feed_titles) + '\n\n'
+    if feed_summary != feed_summary_old or rss_once is False:
+        feed_summary_old = feed_summary
+        rss_once = True
+        yield_me += 'event: rssSum\n' + 'data: ' + json.dumps(feed_summary) + '\n\n'
+    if templateData['icon'] != icon_old:
+        icon_old = templateData['icon']
+        yield_me += 'event: icon\n' + 'data: ' + json.dumps(templateData['icon']) + '\n\n'
+    if forecast_day != forecast_day_old:
+        forecast_day_old = forecast_day
+        yield_me += 'event: forecastDay\n' + 'data: ' + json.dumps(forecast_day) + '\n\n'
+    if forecast_cond != forecast_cond_old:
+        forecast_cond_old = forecast_cond
+        yield_me += 'event: forecastCond\n' + 'data: ' + json.dumps(forecast_cond) + '\n\n'
+    if forecast_high != forecast_high_old:
+        forecast_high_old = forecast_high
+        yield_me += 'event: forecastHigh\n' + 'data: ' + json.dumps(forecast_high) + '\n\n'
+    if forecast_low != forecast_low_old:
+        forecast_low_old = forecast_low
+        yield_me += 'event: forecastLow\n' + 'data: ' + json.dumps(forecast_low) + '\n\n'
 
-        time.sleep(0)
+    yield yield_me
+
+    time.sleep(0)
 
 
 try:
@@ -198,6 +273,8 @@ try:
 
     @app.route('/')
     def my_form():
+        global rss_once
+        rss_once = False
         return render_template("index.html", **templateData)
 
     if __name__ == '__main__':
--- static/sse.js
+++ static/sse.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
     sse = new EventSource('/my_event_source');
+    sse.addEventListener('dayNight', function(message) {
+        $('#test').html(message.data);
+        if (message.data === 'night'){
+            $('#day').fadeOut(1000);
+            $('#clockBoxCoverDay').fadeOut(1000);
+            $( "#clockBox" ).stop(true).animate({backgroundColor: '#141f31', borderColor: '#fff'}, 1000 );
+            $( "#content .ui-state-default a, #tabList .ui-state-default, #tabList .ui-state-default" ).css('background', "#151532");
+            $( "#content .ui-state-active a" ).css('background',"#1f1f3f");
+            $( "#tabList .ui-state-active" ).css('background',"#1f1f3f");
+            $('#tom').css('display','inline-block');
+        } else {
+            $('#day').fadeIn(1000);
+            $( "#clockBox" ).stop(true).animate({backgroundColor: '#c9e3fb', borderColor: '#5382cc'}, 1000 );
+            $('#clockBoxCoverDay').fadeIn(1000);
+            $( "#content .ui-state-default a, #tabList .ui-state-default, #tabList .ui-state-default" ).css('background', "#afc9e1");
+            $( "#content .ui-state-active a").css('background', "#c2ddf5;");
+            $( "#tabList .ui-state-active" ).css('background', "#c2ddf5;");
+            $('#tom').css('display','none');
+        }
+    });
+    // ^^^^^^Figure out why tabs are not switching inistantly^^^^^
     sse.addEventListener('outTemp', function(message) {
         jQuery({someValue: $('#outTemp').html()}).animate({someValue: message.data}, {
             duration: 1000,
@@ -16,9 +37,14 @@ $(document).ready(function() {
                 $('#inTemp').text(Math.ceil(this.someValue));
             }
         })});
-    sse.addEventListener('time', function(message) {
-        $('#clockBoxCover').html(message.data)
-    });
+    sse.addEventListener('tomTemp', function(message) {
+        jQuery({someValue: $('#tomTemp').html()}).animate({someValue: message.data}, {
+            duration: 1000,
+            easing:'swing',
+            step: function() {
+                $('#tomTemp').text(Math.ceil(this.someValue));
+            }
+        })});
     sse.addEventListener('rssTitle', function(message) {
         message = JSON.parse(message.data);
         $('#rss1').html('(1) - ' + message[0] + ' - ');
@@ -65,6 +91,66 @@ $(document).ready(function() {
             $('#front').html('<img src="/static/images/' + message[2] + '" >').css('visibility', 'visible');}
     });
 
+    sse.addEventListener('forecastDay', function(message) {
+        message = JSON.parse(message.data);
+        $('#day1name').html(message[0]);
+        $('#day2name').html(message[1]);
+        $('#day3name').html(message[2]);
+        $('#day4name').html(message[3]);
+        $('#day5name').html(message[4]);
+    });
+    sse.addEventListener('forecastHigh', function(message) {
+        message = JSON.parse(message.data);
+        $('#day1high').html(message[0]);
+        $('#day2high').html(message[1]);
+        $('#day3high').html(message[2]);
+        $('#day4high').html(message[3]);
+        $('#day5high').html(message[4]);
+    });
+    sse.addEventListener('forecastLow', function(message) {
+        message = JSON.parse(message.data);
+        $('#day1low').html(message[0]);
+        $('#day2low').html(message[1]);
+        $('#day3low').html(message[2]);
+        $('#day4low').html(message[3]);
+        $('#day5low').html(message[4]);
+    });
+    var skycons = new Skycons({"color": "white"});
+    sse.addEventListener('forecastCond', function(message) {
+        message = JSON.parse(message.data);
+        var i;
+        for(i in message){
+            switch (message[i]){
+                case 'partlycloudy':
+                case 'mostlysunny':
+                    message[i] = 'partly-cloudy-day';
+                    break;
+                case 'clear':
+                    message[i] = 'clear-day';
+                    break;
+                case 'mostlycloudy':
+                case 'partlysunny':
+                    message[i] = 'cloudy';
+                    break;
+                case 'flurries':
+                    message[i] = 'snow';
+                    break;
+                case 'chancetstorms':
+                    message[i] = 'rain';
+                    break;
+
+            }
+        }
+        skycons.set("icon1", message[0]);
+        skycons.set("icon2", message[1]);
+        skycons.set("icon3", message[2]);
+        skycons.set("icon4", message[3]);
+        skycons.set("icon5", message[4]);
+
+    });
+
+
+    skycons.play();
     $("ul#ticker01").webTicker('update');
 });
 
