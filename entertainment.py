8c7f7618867a0c88a5a0f3e41a2d75f9b72557b8
punksux <punksux2.0@gmail.com>
2014-10-20 19:58:57 -0600

    more

--- entertainment.py
+++ entertainment.py
@@ -2,6 +2,7 @@ from urllib.request import Request, urlopen
 rotten_tomatoes_type = 'opening'
 import json
 from xml.etree import ElementTree as ET
+import datetime
 
 rotten_tomatoes_api = 'j66zchayd6megzhvzhp33dm9'
 rotten_tomatoes_website = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/%s.json?apikey=%s&limit=10' \
@@ -56,6 +57,11 @@ def get_local_events():
         title = i[0].text
         description = i[2].text
         start_time = i[3].text
+        start_time = datetime.datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+        if start_time.strftime('%I:%M') = '00:00':
+            pass
+        else:
+            start_time = start_time.strftime('%B %d @ %I:%M %p')
         stop_time = i[4].text
         venue = i[12].text
         if i[36].find('url') is not None:
