165e24ccd13f30f14c49677ba5e4ba78e2955252
punksux <punksux2.0@gmail.com>
2014-10-29 14:10:58 -0600

    I think I fixed it this time

--- info.py
+++ info.py
@@ -642,7 +642,7 @@ try:
         if on_pi:
             if button == 'p':
                 for proc in psutil.process_iter():
-                    if 'pianobar' in proc['name']:
+                    if 'pianobar' in proc.name():
                         print('pianobar running')
                         with open('/home/pi/.config/pianobar/ctl', 'w') as fp:
                             fp.write(button)
