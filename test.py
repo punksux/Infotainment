a29946764df0fac5392369574f3d45510f4cac72
punksux <punksux2.0@gmail.com>
2014-11-05 13:44:59 -0700

    Fixed lyrics

--- test.py
+++ test.py
@@ -1,7 +1,61 @@
+from flask import Flask, render_template, Response, request, jsonify
+import logging
+import logging.handlers
+from socket import timeout
+import json
+from datetime import datetime, timedelta
+import random
+import time
+from apscheduler.scheduler import Scheduler
+import feedparser
+import sports
+import entertainment
+import platform
+import os
+import re
+import urllib
+from urllib.request import Request, urlopen
+import urllib.error
+from urllib.parse import quote
 from xml.etree import ElementTree as ET
+import os.path
+import requests
 
-t = ET.parse('lyric.xml')
-items = t.getroot()
-lyrics = items[9].text
+album_info = []
 
-print(lyrics)
+
+def get_album(song2, artist2, album2, like2):
+    global album_info
+    last_fm_website = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&' \
+                      'api_key=7e0ead667c3b37eb1ed9f3d16778fe38&artist=%s&album=%s&format=json' \
+                      % (quote(artist2), quote(album2))
+    chartlyrics_website = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?' \
+                          'artist=%s&song=%s' % (quote(artist2), quote(song2))
+
+    # q = urlopen(last_fm_website)
+    # json_string = q.read()
+    # parsed_json = json.loads(json_string.decode('utf-8'))
+    # if 'image' in parsed_json['album']:
+    #     album_art = parsed_json['album']['image'][3]['#text']
+    # else:
+    #     album_art = '/static/images/pandora/blank.jpg'
+    # if 'wiki' in parsed_json['album']:
+    #     album_sum = re.sub('<[^<]+?>', '', parsed_json['album']['wiki']['summary'])
+    # else:
+    #     album_sum = ''
+
+    print('Getting lyrics')
+    t = ET.parse(urlopen(chartlyrics_website))
+    print('1')
+    items = t.getroot()
+    print('2')
+    lyrics = items[9].text
+    print(items[9].text)
+
+
+    album_info = [song2, artist2, album2, '12', '12', like2, lyrics]
+    print(album_info)
+
+
+
+get_album('Smoke Two Joints', 'Sublime', 'Sublime', '')
